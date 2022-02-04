89c7f49c0fe69872ce070c7c532d03a769a72b8b
Emily Yang <78923427+Jou2001@users.noreply.github.com>
2022-02-04 22:10:10 +0800

    Update Permutation Generator.cpp

--- EX1_Fibonacci Series Generator/Permutation Generator.cpp
+++ EX1_Fibonacci Series Generator/Permutation Generator.cpp
@@ -9,10 +9,6 @@
 using namespace std ;
 
 int GetNum() ;
-void loopFindFibonacci( int input ) ;
-void HowManyDivisor( llu num, int serial ) ;
-llu recursionFindFi( int input ) ;
-llu reHowManyDivisor ( llu num, int divisor, int i ) ;
 bool is_Num( string input ) ;
 llu HowMany( int input, int number ) ;
 void find_Per( int input, int several ) ;
@@ -49,22 +45,6 @@ int main() {
     
     if ( cmd == 0 )
       break ;
-      
-    else if ( cmd == 1 ) {  // cmd == 1 use loop to find fibonacci
-      input = GetNum() ;    // get a num that is no more 92
-      loopFindFibonacci( input ) ;
-    } // else if
-                  
-    else if ( cmd == 2 ) { // cmd == 2 use recursion to find fibonacci
-      input = GetNum() ;
-      for( int i = 1 ; i <= input ; i++ ) {
-        inner_times = 0, divisor = 1 ;
-        num[i] = recursionFindFi( i ) ;
-        reHowManyDivisor ( num[i], divisor, i ) ;
-      } // for
-      printf( "< Outer Recursion  %3d times >\n\n", outer_times ) ;
-    } // else if
-    
     
     else if ( cmd == 3 ) {
       input = GetNum() ;
@@ -74,7 +54,6 @@ int main() {
       find_Per( input, several );
       e_nd = clock() ;
       printf( "%f ms\n", (e_nd - start)  ) ;
-      
     } // else if
         
     else
@@ -84,118 +63,7 @@ int main() {
 
   } // main
 
-bool is_Num( string input ) { // check the string input is an integer or not
-  
-  for( int i = 0 ; i < input.size() ; i++ ) { // input.size can return the length of the string input
-    if( input[i] >= '0' && input[i] <= '9' ) // Use ASCII to know whether the input[i] is an integer
-      continue ;
-    else
-      return false ; // String input have an element that is not integer
-  } // for
-  return true ; // In string input all of the elements are an integer
-  
-} // is_Num
 
-int GetNum() { // Get a number from user and make sure the number no more than 92
-  string input ;
-  
-  while(true) {
-    if ( cmd == 3 )
-      cout << "Enter a positive integer agree [2,9]:" ;
-    else
-      cout << "Enter a positive integer no more than 92:" ;
-    cin >> input ;
-    
-    if ( !is_Num( input ) ) // Input is not integer
-      cout << "Not an integer Please try again! " << endl ;
-   
-    else { // Input is an integer
-      int num = stoi(input) ; // convert input from string to int
-      if ( cmd == 3 ) {
-        if( num >= 2 && num <= 9 )
-          return num ;
-        else
-          cout << "Wrong integer Please try again! " << endl ;
-      } // if
-      
-      else {
-        if ( num >= 0 && num <= 92 )
-          return num ;
-        else
-          cout << "Wrong integer Please try again! " << endl ;
-      } // else
-    } // else
-  } // while
-} // GetNum
-
-
-void loopFindFibonacci( int input ) { // findFibonacci
-  
-  num[0] = 1, outer_times = 0 ;
-  
-  for( int i = 1, serial = 1 ; i <= input ; i++, serial++ ) { // calculate Fibonacci
-    
-    if ( i == 1 )
-      num[i] = 1 ;
-    
-    else
-      num[i] = num[i-1] + num[i-2] ;
-    
-    HowManyDivisor( num[i], serial ) ;
-    outer_times++ ;
-    
-  } // for
-  
-  printf( "< Outer loop :    %3dtimes >\n\n", outer_times ) ;
-  
-} // FindFibonacci
-
-void HowManyDivisor ( llu num, int serial ) { // find how many divisors
-  
-  divisor = 1, inner_times = 0, tmp = 0 ;
-  
-  for( divisor = 1 ; ( divisor * divisor ) <= num ; divisor++, inner_times++ ) { // use divisors are pair
-    if ( num % divisor == 0 )
-      tmp = divisor ;
-  } // for
-  
-  printf( "[%3d] %llu = %llu * %llu     ( Inner Loop:  %3d times )  \n", serial, num, tmp, num/tmp, inner_times  ) ;
-    
-} // HowManyDivisor
-
-llu recursionFindFi( int input ) {
-  //outer_times++ ;
-  if ( num[input] ) // If num[input] has already been calculated, return it directly without continuing to calculate
-    return num[input] ;
-  
-  outer_times++ ;
-  
-  if( input == 1 || input == 0 )
-    return  1 ;
-  
-  else
-    return recursionFindFi( input-1 ) + recursionFindFi( input-2 ) ;
-  
-} // recursionFi
-
-llu reHowManyDivisor ( llu num, int divisor, int i ) { // use recuursion to find divisor
-  
-  inner_times++ ;
-  
-  if( ( divisor * divisor ) > num )
-    return -1 ;
-  
-  tmp = reHowManyDivisor( num, ++divisor, i ) ;
-  
-  if( tmp == -1 && num % (divisor-1) == 0 ) {
-   printf( "[%3d] %3llu = %3llu * %3llu     ( Inner Recursuion:  %3d times )  \n", i, num, (divisor-1), num/(divisor-1), inner_times  ) ;
-    return divisor ;
-  } // if
-  
-  return tmp ;
-
-} // reHowManyDivisor
-  
 llu HowMany( int input, int number ) { // calculate how many possible several
   if( input > 0 )
     return (number) * HowMany( --input, number-1 ) ;
@@ -250,7 +118,46 @@ void find_Per( int input, int several ) {
   
 } // find_Per
 
+bool is_Num( string input ) { // check the string input is an integer or not
+  
+  for( int i = 0 ; i < input.size() ; i++ ) { // input.size can return the length of the string input
+    if( input[i] >= '0' && input[i] <= '9' ) // Use ASCII to know whether the input[i] is an integer
+      continue ;
+    else
+      return false ; // String input have an element that is not integer
+  } // for
+  return true ; // In string input all of the elements are an integer
+  
+} // is_Num
 
-
-
-
+int GetNum() { // Get a number from user and make sure the number no more than 92
+  string input ;
+  
+  while(true) {
+    if ( cmd == 3 )
+      cout << "Enter a positive integer agree [2,9]:" ;
+    else
+      cout << "Enter a positive integer no more than 92:" ;
+    cin >> input ;
+    
+    if ( !is_Num( input ) ) // Input is not integer
+      cout << "Not an integer Please try again! " << endl ;
+   
+    else { // Input is an integer
+      int num = stoi(input) ; // convert input from string to int
+      if ( cmd == 3 ) {
+        if( num >= 2 && num <= 9 )
+          return num ;
+        else
+          cout << "Wrong integer Please try again! " << endl ;
+      } // if
+      
+      else {
+        if ( num >= 0 && num <= 92 )
+          return num ;
+        else
+          cout << "Wrong integer Please try again! " << endl ;
+      } // else
+    } // else
+  } // while
+} // GetNum
